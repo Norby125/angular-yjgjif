ae14eb6eee332eb5f0d279c65408bbce18091a9e
Norby125 <norby125@hotmail.com>
2019-06-27 23:23:58 +0200

    4 files modified, 3 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { TableComponent } from './table/table.component';
+import { PersonComponent } from './person/person.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, TableComponent ],
+  declarations: [ AppComponent, TableComponent, PersonComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
-
+import { Component, OnInit, Input, ContentChild, TemplateRef } from '@angular/core';
+import {PersonComponent} from '../person/person.component';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TableComponent implements OnInit {
   @Input() public data;
+  @ContentChild('person', {read: TemplateRef, static: false}) personTemplateRef;
   constructor() { }
 
   ngOnInit() {
